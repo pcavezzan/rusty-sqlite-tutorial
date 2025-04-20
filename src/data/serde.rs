be57d55e479dce1ca1613ed84ab2e6df81723c44
Patrice CAVEZZAN <pcavezzan@gmail.com>
2025-04-20 18:35:44 +0200

    2 - Serialization/Deserialization
    Use custom serialization for primitives in order to simplify cursor usage

--- src/data/serde.rs
+++ src/data/serde.rs
@@ -1,9 +1,9 @@
 use crate::errors::{DeserializationError, SerializationError};
 
 pub trait Serializable {
-    fn serialize(&self, buffer: &mut [u8]) -> Result<(), SerializationError>;
+    fn serialize(&self, cursor: &mut std::io::Cursor<&mut [u8]>) -> Result<(), SerializationError>;
 }
 
 pub trait Deserializable: Sized {
-    fn deserialize(buffer: &[u8]) -> Result<Self, DeserializationError>;
+    fn deserialize(cursor: &mut std::io::Cursor<&[u8]>) -> Result<Self, DeserializationError>;
 }
